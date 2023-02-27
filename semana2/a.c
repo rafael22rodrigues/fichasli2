b378b02011ec8aa7fe538cb05fb1fc3a4c591f56
a96640 <a96640@alunos.uminho.pt>
2023-02-27 22:33:47 +0000

    a.c e b.c alterados

--- semana2/a.c
+++ semana2/a.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <assert.h>
 
-
-
-
-
-
-int main()
-{
-	
-	return 0;
+int main() {
+    int n;
+    assert(scanf("%d", &n) == 1);
+    int i = 0;
+    int a;
+    int array[n];
+    while (i < n){
+        assert(scanf("%d", &a) == 1 );
+        if(a>78){
+            return 0;
+        }
+        array[i] = a;
+        i++;
+    }
+    int x=0, y=0;
+    for (int j = 0; j < n; j++) {
+        if((array[j])%4 == 0){
+            x++;
+        } else if ((array[j])%4 == 1){
+            y--;
+        } else if((array[j])%4 == 2){
+            y++;
+        } else if((array[j])%4 == 3){
+            x--;
+        }
+    }
+    printf("%d %d\n", x, y);
+    return 0;
 }
 
--- semana2/b.c
+++ semana2/b.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <assert.h>
 
+int main() {
+    int c;
+    assert(scanf("%d", &c) == 1);
+    int a;
+    assert(scanf("%d", &a)==1);
+    int n;
+    assert(scanf("%d", &n) == 1);
+    int i = 0, b;
+    int array[n];
+    while (i < n){
+        assert(scanf("%d", &b) == 1);
+        if(b == 1 || b == -1){
+            array[i] = b;
+            i++;
+        } else return 0;
 
+    }
+    int r = c;
+    for (int j = 0; j < n; j++) {
+        if (array[j] == -1 && r > 0){
+            r--;
+        } else if (array[j] == 1 && r < a){
+            r++;
+        }
+    }
+    printf("%d\n", r);
 
-
-
-
-int main()
-{
-	
-	return 0;
+    return 0;
 }
-

c568b1b28a5ae90190f1d12df91adabefb317167
a96640 <a96640@alunos.uminho.pt>
2023-03-01 17:30:06 +0000

    c.c completada

--- semana2/c.c
+++ semana2/c.c
@@ -3,38 +3,34 @@
 #include <string.h>
 
 
-
 int main() {
     int n;
     assert(scanf("%d", &n) == 1);
     int i = 0;
-    char str[3];
     int r = 0, k = 0, l = 0;
-    while (i<n){
-        assert(scanf("%s", &str) == 1);
-        int ret1 = strstr(str, "@*");
-        int ret2 = strstr(str, "@-");
-        int ret3 = strstr(str, "@+");
-        int ret4 = strstr(str, "|*");
-        int ret5 = strstr(str, "|-");
-        int ret6 = strstr(str, "|+");
-        int ret7 = strstr(str, "X*");
-        int ret8 = strstr(str, "X-");
-        int ret9 = strstr(str, "X+");
-        if (ret1 || ret2 || ret3 || ret4 || ret5 || ret6 || ret7 || ret8 || ret9){
-            if (ret1 || ret5 || ret9){
-                r++;
-            }
-            if (ret2 || ret6 || ret7){
-                k++;
-            }
-            if (ret3 || ret4 || ret8){
-                l++;
-            }
-            i++;
+    char s[3];
+    char *a = "@*";
+    char *b = "@-";
+    char *c = "@+";
+    char *d = "|*";
+    char *e = "|-";
+    char *f = "|+";
+    char *g = "X*";
+    char *h = "X-";
+    char *j = "X+";
+
+    do {
+        assert(scanf("%s", s));
+        if (strstr(s,a) || strstr(s,e) || strstr(s,j)){
+            r++;
+        } else if (strstr(s,c) || strstr(s,d) || strstr(s,h)){
+            l++;
+        } else if(strstr(s,b) || strstr(s,f) || strstr(s,g)){
+            k++;
         }
-        else return 0;
-    }
+        i++;
+    } while(i < n);
     printf("%d %d %d\n", l, k, r);
     return 0;
 }
+

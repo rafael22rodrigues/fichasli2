91749ae1d5a1b79d91905206c6c2a1e830edfcfa
a96640 <a96640@alunos.uminho.pt>
2023-03-15 11:51:28 +0000

    documentação de a.c e c.ccompletas

--- semana2/a.c
+++ semana2/a.c
@@ -1,33 +1,85 @@
 #include <stdio.h>
 #include <assert.h>
 
+
+/*
+    Uma taróloga encontra-se perdida no limbo, e para se guiar ela irá usar as cartas que vai de 1 a 78.
+    Caso calhe a carte 1 a coordenada fica (X,Y-1), para a carta 2 fica (X,Y+1), para carta 3 fica (X-1,Y), 4 fica (X+1,Y) e 5 fica (X,Y-1) e assim por adiante... 
+    
+    Este programa serve para determinar as coordenadas finais ao fim de um certo percurso.
+*/
+
 int main() {
-    int n;
-    assert(scanf("%d", &n) == 1);
-    int i = 0;
-    int a;
-    int array[n];
+    int n; // o número de comandos
+    assert(scanf("%d", &n) == 1); // aqui lê-se o número de comandos e através do assert verifica se n é um número inteiro mesmo
+    int i = 0; // admite-se que o número de comandos é um número inteiro positivo maior que 0, caso contrário os ciclos a seguir não se realizam imprimindo no final "0 0"
+    int a; // número do comando correspondente que será alterado ao longo do ciclo a seguir
+    int array[n]; //um array auxiliar do tamanho do número de comandos servirá para armazenar os valores de a ao longo do ciclo a seguir
+    
     while (i < n){
-        assert(scanf("%d", &a) == 1 );
-        if(a>78){
+        assert(scanf("%d", &a) == 1 ); // o valor de a é atualizado sempre que o ciclo reinicia
+        if(a>78 || a<0){ // caso o valor registado em a seja um valor acima de 78 (número máximo das cartas) ou abaixo de 0, o programa termina retornando o valor 0
             return 0;
         }
-        array[i] = a;
-        i++;
+        array[i] = a; // aqui no array é armazenado todos os valores de a ao longo do ciclo (pela mesma ordem que são inseridos)
+        i++; // sempre que é adicionada uma unidade a i o ciclo reinicia até ser igual a n-1
     }
-    int x=0, y=0;
+    int x=0, y=0; // o caminho começa sempre nas coordenadas (0,0)
     for (int j = 0; j < n; j++) {
         if((array[j])%4 == 0){
-            x++;
+            x++; //se a posição j do array for múltiplo de 4, x aumenta uma unidade
         } else if ((array[j])%4 == 1){
-            y--;
+            y--; //se a posição j do array na divisão inteira por 4 der resto 1, y diminui uma unidade
         } else if((array[j])%4 == 2){
-            y++;
+            y++; //se a posição j do array na divisão inteira por 4 der resto 2,  aumenta uma unidade
         } else if((array[j])%4 == 3){
-            x--;
+            x--; //se a posição j do array na divisão inteira por 4 der resto 3, x diminui uma unidade
         }
     }
-    printf("%d %d\n", x, y);
+    printf("%d %d\n", x, y); //imprime-se as coordenadas finais
     return 0;
 }
+
+/*
+
+Input:
+6
+21
+42
+54
+56
+54
+4
+
+Output:
+2 2
+
+Input:
+10
+54
+42
+21
+45
+64
+33
+51 
+75
+38
+74
+
+Output:
+-1 1
+
+Input:
+3
+67
+60
+30
+
+Output:
+0 1
+
+*/
+
+
 
--- semana2/c.c
+++ semana2/c.c
@@ -2,13 +2,26 @@
 #include <assert.h>
 #include <string.h>
 
+/*
+    Pretende-se pôr duas IA's de arquitetura inzel e cmd a simular um jogo de pedra, papel, tesoura.
+    O programa serve para armazenar os resultados obtidos ao longo das partidas.
+
+    A representação dos simbolos que representam o pedra, papel, tesoura consegue ser visto na tabela abaixo
+
+        Inzel    Cmd
+Pedra    @        *
+Papel    |        -
+Tesoura  X        +
+*/
+
 
 int main() {
-    int n;
+    int n; // número de comandos
     assert(scanf("%d", &n) == 1);
-    int i = 0;
-    int r = 0, k = 0, l = 0;
-    char s[3];
+    int i = 0;// admite-se que o número de comandos é um número inteiro positivo maior que 0, caso contrário o ciclo "do-while" a seguir não é realizado imprimindo no final "0 0 0"
+    int r = 0, k = 0, l = 0; // r, k e l representam as vezes as arquiteturas empataram, a cmd ganhou e a que a inzel ganhou, respetivamente
+    char s[3]; //string que será lida e atualizada no ciclo "do-while" a seguir
+    //as próximas strings de a a j são todas as combinações possíveis que pode sair de um jogo de pedra papel tesoura entre as arquiteturas inzel e cmd
     char *a = "@*";
     char *b = "@-";
     char *c = "@+";
@@ -20,17 +33,60 @@ int main() {
     char *j = "X+";
 
     do {
-        assert(scanf("%s", s));
+        assert(scanf("%s", s)); //lê uma string
+        /*
+            com a função strstr já definida em string.h verifica se uma das strings constantes definidas acima são iguais à string que acabou de ser inserida
+            Caso a string lida não seja igual a uma das constantes definidas acima os valores de r, k e l não serão atualizados
+        */
         if (strstr(s,a) || strstr(s,e) || strstr(s,j)){
-            r++;
+            r++; //caso as arquiteturas empatem r aumenta uma unidade 
         } else if (strstr(s,c) || strstr(s,d) || strstr(s,h)){
-            l++;
+            l++; //caso a arquiteturas inzel ganhe l aumenta uma unidade 
         } else if(strstr(s,b) || strstr(s,f) || strstr(s,g)){
-            k++;
+            k++; //caso a arquitetura cmd ganhe k aumenta uma unidade 
         }
         i++;
     } while(i < n);
-    printf("%d %d %d\n", l, k, r);
+    printf("%d %d %d\n", l, k, r); // imprime-se os resultados finais
     return 0;
 }
+
+/*
+Input:
+10
+@-
+X+
+|-
+@*
+@+
+X*
+X-
+|*
+|+
+@+
+
+
+Output:
+4 3 3
+
+Input:
+7
+@*
+@-
+|+
+X-
+X*
+X- 
+X+
+
+
+Output:
+2 3 2
+
+*/
+
+
+
+
+
 

affc847712fb07f254f13ff697f4d9a64c7387be
a96640 <a96640@alunos.uminho.pt>
2023-03-30 23:20:29 +0100

    b.c completo

--- semana6/b.c
+++ semana6/b.c
@@ -24,7 +24,7 @@ int abs (int x){
 void bubblepeso (Cozinheiro *a, int N) {
     int i;
     for (i = 1; i < N; i++) {
-        if (abs(90 - a[i].peso) <= abs(90 - a[i - 1].peso) ) {
+        if (abs(90 - a[i].peso) < abs(90 - a[i - 1].peso)){
             swapCozinheiro(a, i - 1, i);
         }
     }
@@ -33,29 +33,33 @@ void bubblepeso (Cozinheiro *a, int N) {
 void bubblealtura(Cozinheiro *a, int N){
     int j;
     for (j = 1; j < N; j++) {
-        if((abs(90-a[j].peso) == abs(90-a[j-1].peso)) && (a[j].altura >=a[j-1].altura)){
+        if((abs(90-a[j].peso) == abs(90-a[j-1].peso)) && (a[j].altura > a[j-1].altura)){
             swapCozinheiro(a,j-1,j);
+        }
+    }
+}
+
+void bubblenomes(Cozinheiro *a, int N){
+    for (int l = 1; l < N; l++) {
+        if (abs(90 - a[l].peso) == abs(90 - a[l-1].peso) && a[l].altura == a[l-1].altura){
+            int r = strcmp(a[l].nome,a[l-1].nome);
+            if (r<0){
+                swapCozinheiro(a,l-1,l);
+            }
         }
     }
 }
 
 void ordenar (Cozinheiro *a, int N){
     int i, j;
-    int pesos[N];
     for (i = N; i > 0; i--) {
         bubblepeso(a,i);
     }
     for (j = N; j > 0; j--) {
         bubblealtura(a,j);
     }
-
-    for (int l = 0; l < N-1; l++) {
-        if (a[l].peso == a[l+1].peso && a[l].altura == a[l+1].altura){
-            int r = strcmp(a[l].nome,a[l+1].nome);
-            if (r>0){
-                swapCozinheiro(a,l+1,l);
-            }
-        }
+    for (int l = N; l > 0; l--) {
+        bubblenomes(a,l);
     }
 }
 
